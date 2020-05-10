8fbc0684ae5fd9a3d6cd97d62b45cde0e19551d7
NathanRoyer <nathan.royer.pro@gmail.com>
2020-05-10 23:44:35 +0200

    - text-utils now creates a class: "Text" - all text operations have corresponding methods in this class

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const textUtils = require('./text-utils');
 const crypto = require('crypto');
 const app = express();
+// The following snippet adds the
+// request body to the request object
 app.use((req, res, next) => {
 	if (req.is('text/*')) {
 		req.body = '';
@@ -10,13 +12,23 @@ app.use((req, res, next) => {
 		req.on('end', next);
 	} else next();
 });
+// --- HTTP ---
 const port = 8080;
-const maxWordsPerToken = 100;// 80000;
-const chunkLength = 80;
 const tokenHeaderName = 'token';
-const oneDayInMilliseconds = 1000 * 60;// * 60 * 24;
-//                           1s     1mn  1h   1d
 
+// --- Justification ---
+const chunkLength = 80;
+
+// --- Users & Tokens ---
+/* testing values:*/
+const maxWordsPerToken = 100;
+const oneDayInMilliseconds = 1000 * 60; // actually 1mn
+
+/* correct values:
+const maxWordsPerToken = 80000;
+const oneDayInMilliseconds = 1000 * 60 * 60 * 24;
+//                           1s     1mn  1h   1d
+ */
 var users = {  };
 
 class User {
@@ -30,7 +42,6 @@ class User {
 		var success = (this.usedWords + wordCount) <= this.availableWords;
 		if (!success){
 			let elapsedMs = Date.now() - this.lastReset;
-			console.log({elapsedMs});
 			if (elapsedMs > oneDayInMilliseconds){
 				success = true;
 				this.usedWords = 0;
@@ -50,7 +61,7 @@ function newTokenHandler(req, res){
 		let tokenId = crypto.randomBytes(16).toString('hex');
 		if (users[tokenId] !== undefined){
 			// unfortunate collision, let's try again
-			return newTokenEndpoint(req, res);
+			return newTokenHandler(req, res);
 		} else {
 			users[tokenId] = new User(param.email, maxWordsPerToken);
 			console.log('new user:', tokenId, 'with email =', param.email)
@@ -66,20 +77,25 @@ function justifyHandler(req, res){
 	if (toJustify === undefined){
 		res.status(400).send("Error 400: Bad Request<br>Invalid request body");
 	} else {
-		var state = textUtils.generateState(toJustify);
-		textUtils.removeDoubleLines(state);
-		textUtils.removeDoubleSpaces(state);
-		console.log("words:", state.wordCount);
-
 		let tokenId = req.get(tokenHeaderName);
 		var user = users[tokenId];
 		if (user === undefined){
 			res.status(400).send("Error 400: Bad Request<br>Invalid token");
-		} else if (user.canUseWords(state.wordCount)){
-			res.send(textUtils.justify(toJustify, chunkLength));
-			user.useWords(state.wordCount);
 		} else {
-			res.status(402).send("Error 402: Payment Required");
+			var text = new textUtils.Text(toJustify);
+			text.removeDoubleLines();
+			text.removeDoubleSpaces();
+			console.log("User ", user.email, "wants to use", text.wordCount, "words.");
+
+			if (user.canUseWords(text.wordCount)){
+				text.justify(chunkLength);
+				user.useWords(text.wordCount);
+				res.send(text.text);
+				console.log("Accepted.");
+			} else {
+				res.status(402).send("Error 402: Payment Required");
+				console.log("Denied.");
+			}
 		}
 	}
 }
