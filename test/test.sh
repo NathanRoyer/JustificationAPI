b74a7b8cb19372fb26ace4771bdc987054047037
NathanRoyer <nathan.royer.pro@gmail.com>
2020-05-10 23:46:51 +0200

    - 100% english speaking test.sh

--- test/test.sh
+++ test/test.sh
@@ -2,39 +2,39 @@ HOST=http://localhost:8080/api
 TOKEN=$(curl -s -X POST -H "Content-Type: text/plain" -d @test/email-for-token.txt $HOST/token)
 echo "Token: $TOKEN"
 echo "Testing user limits"
-echo "Essai 1"
+echo "Request #1"
 curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
-echo "Essai 2"
+echo "Request #2"
 curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
-echo "Essai 3"
+echo "Request #3"
 curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
 sleep 5s
 sleep 1m
-echo "Essai 4 apres une minute"
+echo "Request #4 after 1mn"
 curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
-echo "Essai 5"
+echo "Request #5"
 curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
-echo "Essai 6"
+echo "Request #6"
 curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
-echo "Essai 7"
+echo "Request #7"
 curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
 sleep 2m
-echo "Essai 8 apres deux minute"
+echo "Request #8 after 2mn"
 curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
-echo "Essai 9"
+echo "Request #9"
 curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
-echo "Essai 10"
+echo "Request #10"
 curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
-echo "Essai 11"
+echo "Request #11"
 curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
