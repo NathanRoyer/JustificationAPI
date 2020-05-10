750ed0a150d4792f99257caa4a738880359ca375
NathanRoyer <nathan.royer.pro@gmail.com>
2020-05-10 23:17:46 +0200

    - package reconfiguration

--- test/test.sh
+++ test/test.sh
@@ -1,39 +1,40 @@
 HOST=http://localhost:8080/api
-TOKEN=$(curl -s -X POST -H "Content-Type: text/plain" -d @email-for-token.txt $HOST/token)
+TOKEN=$(curl -s -X POST -H "Content-Type: text/plain" -d @test/email-for-token.txt $HOST/token)
 echo "Token: $TOKEN"
+echo "Testing user limits"
 echo "Essai 1"
-curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @input-to-justify.txt $HOST/justify
+curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
 echo "Essai 2"
-curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @input-to-justify.txt $HOST/justify
+curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
 echo "Essai 3"
-curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @input-to-justify.txt $HOST/justify
+curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
 sleep 5s
 sleep 1m
 echo "Essai 4 apres une minute"
-curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @input-to-justify.txt $HOST/justify
+curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
 echo "Essai 5"
-curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @input-to-justify.txt $HOST/justify
+curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
 echo "Essai 6"
-curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @input-to-justify.txt $HOST/justify
+curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
 echo "Essai 7"
-curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @input-to-justify.txt $HOST/justify
+curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
 sleep 2m
 echo "Essai 8 apres deux minute"
-curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @input-to-justify.txt $HOST/justify
+curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
 echo "Essai 9"
-curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @input-to-justify.txt $HOST/justify
+curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
 echo "Essai 10"
-curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @input-to-justify.txt $HOST/justify
+curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
 echo "Essai 11"
-curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @input-to-justify.txt $HOST/justify
+curl -s -X POST -H "Content-Type: text/plain" -H "token: $TOKEN" -d @test/input-to-justify.txt $HOST/justify
 echo
